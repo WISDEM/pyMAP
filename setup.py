6005515fc681f5f56fd7c1d107077a55c124a1ac
Garrett Barter <garrett.barter@nrel.gov>
2020-02-26 13:49:25 -0700

    cleanup of setup file

--- setup.py
+++ setup.py
@@ -6,44 +6,26 @@ from numpy.distutils.core import setup, Extension
 import os
 import sys
 import platform
+import glob
 
-path = 'src' + os.sep + 'pymap'
-bstring_src = ['bstraux.c', 'bstrlib.c']
-cminpack_src = ['dpmpar.c', 'enorm.c', 'enorm_u.c',
-                'lmder.c', 'lmpar.c', 'qrfac.c',
-                'qrsolv.c']
-simclist_src = ['simclist.c']
-src = ['freedata.c', 'jacobian.c', 'lineroutines.c', 'lmroutines.cc',
-       'mapapi.c', 'maperror.c', 'mapinit.c', 'numeric.c', 'outputstream.c',
-       'residual.c']
-for ifile in range(len(bstring_src)):
-    bstring_src[ifile] = os.path.join(path, 'bstring', bstring_src[ifile])
-for ifile in range(len(cminpack_src)):
-    cminpack_src[ifile] = os.path.join(path, 'cminpack', cminpack_src[ifile])
-for ifile in range(len(simclist_src)):
-    simclist_src[ifile] = os.path.join(path, 'simclist', simclist_src[ifile])
-for ifile in range(len(src)):
-    src[ifile] = os.path.join(path, src[ifile])
-src.extend(bstring_src)
-src.extend(cminpack_src)
-src.extend(simclist_src)
-
-include_dirs = ['bstring','cminpack','lapack','simclist']
-for idir in range(len(include_dirs)):
-    include_dirs[idir] = os.path.join(path, include_dirs[idir])
-
+os.environ['NPY_DISTUTILS_APPEND_FLAGS'] = '1'
 
 if platform.system() == 'Windows':
     # For Anaconda
-    cflags = ['-g', '-O1', '-m64', '-fPIC', '-std=c99','-DCMINPACK_NO_DLL']
+    cflags = ['-O1', '-m64', '-fPIC', '-std=c99','-DCMINPACK_NO_DLL']
 elif sys.platform == 'cygwin':
-    cflags = ['-g', '-O1', '-m64', '-fPIC', '-std=c99']
+    cflags = ['-O1', '-m64', '-fPIC', '-std=c99']
 elif platform.system() == 'Darwin':
-    cflags = ['-g', '-O1', '-m64', '-fno-omit-frame-pointer', '-fPIC']#, '-std=c99']
+    cflags = ['-O1', '-m64', '-fno-omit-frame-pointer', '-fPIC']#, '-std=c99']
 else:
-    #cflags = ['-g', '-O1', '-m64', '-fPIC', '-std=c99', '-D WITH_LAPACK']
-    cflags = ['-g', '-O1', '-m64', '-fPIC', '-std=c99']
+    #cflags = ['-O1', '-m64', '-fPIC', '-std=c99', '-D WITH_LAPACK']
+    cflags = ['-O1', '-m64', '-fPIC', '-std=c99']
 
+pymapExt   = Extension('_libmap', sources=glob.glob(os.path.join('src','pymap','**','*.c'), recursive=True)+
+                       glob.glob(os.path.join('src','pymap','**','*.cc'), recursive=True),
+                       extra_compile_args=cflags,
+                       include_dirs=[os.path.join('src','pymap','lapack')])
+    
 setup(
     name='pyMAP',
     version='1.0.0',
@@ -55,8 +37,7 @@ setup(
     py_modules=['pymap'+os.sep+'pymap'],
     package_data={'pymap': []},
     packages=['pymap'],
-    ext_modules=[Extension('_libmap', sources=src, extra_compile_args=cflags,
-                           include_dirs=include_dirs)],
+    ext_modules=[pymapExt],
     zip_safe=False
 )
 
